f22111c7a851ff4919e2df7ed1f412309806a8eb
alisaribas <alisaribash35@gmail.com>
2022-03-08 00:25:23 +0200

    daire hesaplamaları

--- src/Main.java
+++ src/Main.java
@@ -9,27 +9,14 @@ public class Main {
      *
      */
     public static void main(String[] args) {
-        Scanner inp = new Scanner(System.in);
-        int a ,b ,c, u;
-        System.out.print("Üçgenin 1. kenarını giriniz: ");
-        a = inp.nextInt();
-        System.out.print("Üçgenin 2. kenarını giriniz: ");
-        b = inp.nextInt();
-        System.out.print("Üçgenin 3. kenarını giriniz: ");
-        c = inp.nextInt();
+    Scanner inp = new Scanner(System.in);
+    int r,a;
+    double pi = 3.14;
+        System.out.print("Dairenin yarıçapını giriniz: ");
+        r = inp.nextInt();
+        System.out.print("Alfa merkez açısını giriniz: ");
+        a= inp.nextInt();
 
-
-
-        u = (a+b+c)/2;
-
-
-
-        System.out.println("Üçgenin çevresi: " + (2*u));
-
-        System.out.println("Üçgenin alanı: " + (int)Math.sqrt(u*(u-a)*(u-b)*(u-c)) );
-
-
-
-
+        System.out.println("Daire diliminin alanı: " + (pi*(r*r)*a/360) );
     }
 }
