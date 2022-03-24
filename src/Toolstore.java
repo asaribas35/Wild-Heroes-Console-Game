f1b1f26ea0466e49bdaffabe9f267440d1c4a4a1
alisaribas <alisaribash35@gmail.com>
2022-03-24 14:57:47 +0200

    add market

--- src/Toolstore.java
+++ src/Toolstore.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Toolstore extends NormalLoc{
     public Toolstore(Player player,String name){
         super(player,name);
@@ -9,13 +11,47 @@ public class Toolstore extends NormalLoc{
     }
     void menu(){
         System.out.println(super.getName()+"a Hoşgeldiniz ---------------------------");
+        System.out.println("bakiye: " + getPlayer().getMoney());
         System.out.println("1. Ok-Yay : 20 ");
         System.out.println("2. Kadim Kılıç : 30 ");
         System.out.println("3. Mızrak : 35 ");
         System.out.println("4. Çıkış ");
-
+        buy();
     }
     void buy(){
-        System.out.println("aldın");
+        Scanner inp = new Scanner(System.in);
+        int c = inp.nextInt();
+        switch (c){
+            case 1:
+                if (getPlayer().getMoney()>=20){
+                    System.out.println("Ok-Yay aldın");
+                    getPlayer().setMoney(getPlayer().getMoney()-20);
+                }else {
+                    System.out.println("Para yetersiz");
+                }
+                menu();
+            case 2:
+                if (getPlayer().getMoney()>=30){
+                    System.out.println("Kadim Kılıç aldın");
+                    getPlayer().setMoney(getPlayer().getMoney()-30);
+                }else {
+                    System.out.println("Para yetersiz");
+                }
+                menu();
+            case 3:
+                if (getPlayer().getMoney()>=35){
+                    System.out.println("Mızrak aldın");
+                    getPlayer().setMoney(getPlayer().getMoney()-35);
+                }else {
+                    System.out.println("Para yetersiz");
+                }
+                menu();
+            case 4:
+                break;
+            default:
+                System.out.println("hata");
+                break;
+
+        }
     }
 }
